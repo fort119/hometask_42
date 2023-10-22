1b073b3de0913dabc085ca4145fa8b1769de796c
Ivan <i6316434@gmail.com>
2023-10-22 23:53:08 +0300

    ht42 is done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import React from 'react';
+import * as Yup from 'yup';
+import { Formik, Form, Field } from 'formik';
 import './App.css';
+
+const regPhone = /^(\d{0,4})?(\(?\d{3}\)?)(\(?\d{3}\)?)?(\(?\d{4}\)?)$/;
+
+const validationScheme = Yup.object().shape({
+  userName: Yup.string()
+  .required("Required"),
+
+  email: Yup.string()
+  .required("Required")
+  .email("Invalid email"),
+
+  phoneNumber: Yup.string()
+  .matches(regPhone, 'invalid phone number')
+  .min(12, "phone number must contain 12 characters")
+  .max(12, "phone number must contain 12 characters").matches(regPhone, "it's not a phone number").required("Required"),
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1 className='form-name'>Log in!</h1>
+      <Formik
+        validationSchema={validationScheme}
+        initialValues={{
+          userName: "",
+          email: "",
+          phoneNumber: "",
+        }}
+        onSubmit={values =>{
+          const alertText = `your user name is: ${values.userName}\nyour email is: ${values.email}\nyour phone number is: ${values.phoneNumber}`;
+          alert(alertText);
+        }}
         >
-          Learn React
-        </a>
-      </header>
+        
+        {({ errors, touched }) => (
+
+         <Form className = "form">
+
+           <Field name="userName" placeholder = "User name" />
+           {errors.userName && touched.userName ? (
+             <div>{errors.userName}</div>
+           ) : null}
+
+           <Field name="email" type="email" placeholder = "Email" />
+           {errors.email && touched.email ? (
+             <div>{errors.email}</div>
+           ) : null}
+
+           <Field name="phoneNumber" placeholder = "Phone number"/>
+           {errors.phoneNumber && touched.phoneNumber ? (
+             <div>{errors.phoneNumber}</div>
+           ) : null}
+
+           <button type="submit-btn" className='submit-btn'>Submit</button>
+
+         </Form>
+       )}
+
+      </Formik>
     </div>
   );
 }
